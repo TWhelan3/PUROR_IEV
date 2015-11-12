bb82ea7fda569e6c672bbf5ed7f75bcc899b0069
Tim Whelan <twhelan5@uwo.ca>
2015-11-12 16:40:16 -0500

    Add Dec, fixed slice(relied on image index), made Contrast more dynamic, added roles to IEV combining gadget, force messages to have meta date in UnwrapGadget

---
+++ DecImageIndex.cpp
@@ -0,0 +1,32 @@
+//DecImageIndex.cpp
+//Written by Tim Whelan 2015
+//Input ImageHeader-> ???
+//Output ImageHeader-> ??? 
+#include "DecImageIndex.h"
+
+namespace Gadgetron{
+
+int DecImageIndex::process_config(ACE_Message_Block* mb)
+{
+	//this gadget will be very fast, putting a high water mark would 
+	//do nothing unless the next one is very slow and did not address backlog
+	//in future might want to check read xml in this function
+	
+	return GADGET_OK;
+}
+
+int DecImageIndex::process(GadgetContainerMessage< ISMRMRD::ImageHeader>* m1)
+{
+
+	m1->getObjectPtr()->image_index=m1->getObjectPtr()->image_index-1;
+
+	if(-1==this->next()->putq(m1))
+	{
+		m1->release();
+		GERROR("Unable to pass on message\n");
+		return GADGET_FAIL;
+	}
+	return GADGET_OK;
+}
+GADGET_FACTORY_DECLARE(DecImageIndex)
+}
---
+++ DecImageIndex.h
@@ -0,0 +1,21 @@
+#ifndef DecImageIndex_H_
+#define DecImageIndex_H_
+
+#include "PUROR.h"
+
+namespace Gadgetron{
+
+	class EXPORTGADGETSPUROR DecImageIndex:
+	public Gadget1<ISMRMRD::ImageHeader>
+    	{
+	
+	public:
+		GADGET_DECLARE(DecImageIndex);
+
+		virtual int process_config(ACE_Message_Block*);
+		virtual int process(GadgetContainerMessage< ISMRMRD::ImageHeader>* m1);
+	};
+
+}
+
+#endif
