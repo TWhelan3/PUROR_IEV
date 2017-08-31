876baa2559597c8e10c82947a1290adddd661f57
Tim Whelan <twhelan5@uwo.ca>
2017-08-31 15:08:42 -0400

    whitespace cleanup

--- AddMetaData.cpp
+++ AddMetaData.cpp
@@ -1,7 +1,7 @@
 //AddMetaData.cpp
 //Written by Tim Whelan 2015
 //Input ImageHeader-> ???
-//Output ImageHeader-> ??? 
+//Output ImageHeader-> ???
 #include "AddMetaData.h"
 #include "DicomFinishGadget.h"
 namespace Gadgetron{
@@ -10,15 +10,15 @@ int AddMetaData::process_config(ACE_Message_Block* mb)
 {
 	//taken from DicomFixMetaData
 	ISMRMRD::IsmrmrdHeader hdr;
-        deserialize(mb->rd_ptr(), hdr);
-        
-        //Fix incorrectly stored parameters
-        //pixel spacing
-        ISMRMRD::EncodingSpace r_space = hdr.encoding[0].reconSpace;
-        pixel_spacing_X = r_space.fieldOfView_mm.x / r_space.matrixSize.x;
-        pixel_spacing_Y = r_space.fieldOfView_mm.y / r_space.matrixSize.y;
-        
-        slice_spacing = r_space.fieldOfView_mm.z / r_space.matrixSize.z;
+	deserialize(mb->rd_ptr(), hdr);
+
+	//Fix incorrectly stored parameters
+	//pixel spacing
+	ISMRMRD::EncodingSpace r_space = hdr.encoding[0].reconSpace;
+	pixel_spacing_X = r_space.fieldOfView_mm.x / r_space.matrixSize.x;
+	pixel_spacing_Y = r_space.fieldOfView_mm.y / r_space.matrixSize.y;
+
+	slice_spacing = r_space.fieldOfView_mm.z / r_space.matrixSize.z;
 
 	return GADGET_OK;
 }
@@ -32,20 +32,20 @@ int AddMetaData::process(GadgetContainerMessage<DcmFileFormat> * m1)
 	GadgetContainerMessage<ISMRMRD::MetaContainer>* meta;
 
 	if(dcm)
-	{	
+	{
 		f=AsContainerMessage<std::string>(m1->cont());
 	}
 	else
 	{	GERROR("No filename set for DICOM file\n");
 		return GADGET_FAIL;
 	}
-	
+
 	if(f)
-	{	
+	{
 		meta= AsContainerMessage<ISMRMRD::MetaContainer>(f->cont());
 	}
 	else
-	{	
+	{
 		GERROR("No meta data found for DICOM\n");
 		return GADGET_FAIL;
 	}
@@ -57,7 +57,7 @@ int AddMetaData::process(GadgetContainerMessage<DcmFileFormat> * m1)
 	}
 
 	unsigned int BUFSIZE = 1024;
-        char *buf = new char[BUFSIZE];
+	char *buf = new char[BUFSIZE];
 	const char* checkbuf;
 	OFCondition status;
 	DcmTagKey key;
@@ -69,29 +69,25 @@ int AddMetaData::process(GadgetContainerMessage<DcmFileFormat> * m1)
 
 	static bool studyUIDmade=false;
 	std::time_t rawtime;
-         std::time(&rawtime);
-         std::tm *timeinfo = std::localtime(&rawtime);
+	std::time(&rawtime);
+	std::tm *timeinfo = std::localtime(&rawtime);
 
 	if(meta->getObjectPtr()->exists("GADGETRON_IMAGE_SCALE_OFFSET") && meta->getObjectPtr()->exists("GADGETRON_IMAGE_SCALE_RATIO"))
 	{
 		rescaleIntercept=meta->getObjectPtr()->as_double(GADGETRON_IMAGE_SCALE_OFFSET);
-		//rescaleIntercept=meta->getObjectPtr()->as_double(GADGETRON_IMAGE_SCALE_OFFSET);
-	
-		 // Window Center
+
+		// Window Center
 		key.set(0x0028, 0x1050);
 		dataset->remove(key);
-		
+
 		// Window Width
 		key.set(0x0028, 0x1051);
 		dataset->remove(key);
 
-	
-	
-
 		rescaleIntercept = -1.0*rescaleIntercept*rescaleSlope;
-	
+
 		rescaleSlope= 1.0/rescaleSlope;
-	
+
 		key.set(0x0028,0x1052);
 		ACE_OS::snprintf(buf, BUFSIZE, "%f", rescaleIntercept);//
 		write_dcm_string(dataset,key, buf);
@@ -101,13 +97,13 @@ int AddMetaData::process(GadgetContainerMessage<DcmFileFormat> * m1)
 		write_dcm_string(dataset,key, buf);
 
 
-		key.set(0x0028, 0x0030);
+		key.set(0x0028, 0x0030); //Not sure this needs to be in this conditional
 		ACE_OS::snprintf(buf, BUFSIZE, "%.6f\\%.6f", pixel_spacing_Y, pixel_spacing_X);
 		write_dcm_string(dataset,key, buf);
 	}
 
 	key.set(0x0008,0x1030); //Study Description
-	
+
 	dataset->findAndGetString(key, checkbuf, false);
 	if(checkbuf==NULL || !strcmp(checkbuf, "XXXXXXXX"))
 	{
@@ -115,41 +111,33 @@ int AddMetaData::process(GadgetContainerMessage<DcmFileFormat> * m1)
 		write_dcm_string(dataset,key, buf);
 	}
 
-
 	key.set(0x0008,0x103E); //Series Description
-	//if(!dataset->tagExistsWithValue(key))
-	//{
-		std::string type;
-		
-		if(meta->getObjectPtr()->exists("GADGETRON_DATA_ROLE"))
-			type=meta->getObjectPtr()->as_str(GADGETRON_DATA_ROLE);
-		else
-			type="MRI_Images";
-	
-		ACE_OS::snprintf(buf, BUFSIZE, "%s", type.c_str());
-		write_dcm_string(dataset,key, buf);
-	//}
 
-	 
+	std::string type;
+	if(meta->getObjectPtr()->exists("GADGETRON_DATA_ROLE"))
+		type=meta->getObjectPtr()->as_str(GADGETRON_DATA_ROLE);
+	else
+		type="MRI_Images";
+
+	ACE_OS::snprintf(buf, BUFSIZE, "%s", type.c_str());
+	write_dcm_string(dataset,key, buf);
 	key.set(0x0020,0x0010);//Study ID
 	dataset->findAndGetString(key, checkbuf, false);
 	if(checkbuf==NULL || !strcmp(checkbuf, "XXXXXXXX"))
 	{
-		
-			write_dcm_string(dataset,key, "1");
-		
+		write_dcm_string(dataset,key, "1");
 		// be sure to use the same one for all series you generate
 	}
-	
-	//Study UID should be created in IEVChannelSumGadget. 
+
+	//Study UID should be created in IEVChannelSumGadget.
 	key.set(0x0020,0x000D);//Study UID
 	dataset->findAndGetString(key, checkbuf, false);
 	if(checkbuf==NULL || !strcmp(checkbuf, "XXXXXXXX")) //X's are put there by DicomFinish
 	{
 		if(meta->getObjectPtr()->exists("StudyInstanceUID"))
 			write_dcm_string(dataset,key, meta->getObjectPtr()->as_str("StudyInstanceUID"));
-			
-		// If there's a better UID in meta, use it	
+
+		// If there's a better UID in meta, use it
 		// be sure to use the same one for all series you generate
 	}
 
@@ -161,52 +149,47 @@ int AddMetaData::process(GadgetContainerMessage<DcmFileFormat> * m1)
 	{
 		write_dcm_string(dataset,key, buf);
 	}
-	
+
 	key.set(0x0008,0x0030);//Study Time
 	dataset->findAndGetString(key, checkbuf, false);
 	if(checkbuf==NULL || !strcmp(checkbuf, "121212"))
 	{
 		write_dcm_string(dataset,key, buf);
 	}
-	
+
 	key.set(0x0008,0x0021);//Series Date
 	if(!dataset->tagExistsWithValue(key))
 	{
 		write_dcm_string(dataset,key, buf);
 	}
 
-	key.set(0x0008,0x0012);//Instance Creation Date		
+	key.set(0x0008,0x0012);//Instance Creation Date
 	if(!dataset->tagExistsWithValue(key))
 	{
 		write_dcm_string(dataset,key, buf);
-	}	
+	}
 	std::strftime(buf, 100, "%H%M%S", timeinfo);
-
-
 
 	key.set(0x0008,0x0031);//Series Time
 	if(!dataset->tagExistsWithValue(key))
 	{
 		write_dcm_string(dataset,key, buf);
-	}	
-
+	}
 
 	key.set(0x0008,0x0013);//Instance Creation Time
 	if(!dataset->tagExistsWithValue(key))
 	{
 		write_dcm_string(dataset,key, buf);
-	}	
+	}
 
 	key.set(0x0018,0x0081);//Echo Time
 
-	write_dcm_string(dataset,key, meta->getObjectPtr()->as_str("TE"));
-
-
+	if(meta->getObjectPtr()->exists("TE"))
+	{
+		write_dcm_string(dataset,key, meta->getObjectPtr()->as_str("TE"));
+	}
 
 	delete[] buf;
-
-	
-	//add try catch 
 
 	if(-1==this->next()->putq(m1))
 	{
